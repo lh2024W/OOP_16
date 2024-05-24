77e200a0b5675481e2d0c00ce5780eacc06ff1de
Лилия Хачатрян <163654259+lh2024W@users.noreply.github.com>
2024-05-24 22:11:59 +0300

    Update OOP_16.cpp

--- OOP_16/OOP_16.cpp
+++ OOP_16/OOP_16.cpp
@@ -1,223 +1,272 @@
-// OOP_16.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-
 class Ability abstract {};
 
-class Good abstract {
-    SuperHero* SuperHero;
+// быть хорошим, или плохим - это интерфейсы, там указатель на супер-героя не обязателен
+__interface IGood {
+    void DoNiceThings();
 };
 
-class Bad abstract {
-    SuperHero* SuperHero;
+__interface IBad {
+    void DoBadThings();
 };
 
-class Human abstract {
-    SuperHero* SuperHero;
+class Race abstract {}; // раса
+
+// людей, зверей, богов и ТД можно вынести в своеобразную расу
+class Human abstract : public Race {
+    // SuperHero* SuperHero; // тут тоже самое, указатель на супер-героя не нужен
 };
 
-class Animal abstract {
-    SuperHero* SuperHero;
+class Animal abstract : public Race {
+    // SuperHero* SuperHero;
 };
 
-class God abstract {
-    SuperHero* SuperHero;
+class God abstract : public Race {
+    // SuperHero* SuperHero;
 };
 
 class SuperHero abstract {
-    vector <SuperHero*> superheroes;
+    // тут скорее всего будут различные умения и сверх-способности
+    vector <Ability*> abilities;
 public:
-    void AddSuperHero(SuperHero* superhero) {
-        superheroes.push_back(superhero);
+    void AddAbility(Ability* ability) {
+        abilities.push_back(ability);
     }
     ~SuperHero() {
-        for (auto superhero : superheroes) {
-            delete superhero;
-        }
+        // ability будет уничтожаться отдельно, вне текущего класса
+        // тело в деструкторе не нужно
     }
 };
 
 ////////////////////////////////////////////////////////
 
-class IFlyable : public Ability {  // летающий (Тони Старк)
+class IFlyable : public Ability {  // летающий (например, Тони Старк)
 public:
     virtual void Fly() = 0; // летать
 };
 
 class ISuperStrongable : public Ability {  // суперсильный (Халк) (Тони Старк)
 public:
-    virtual void WeightLifting() = 0; //поднятие тяжестей
+    virtual void WeightLifting() = 0; // поднятие тяжестей
 };
 
 class ISuperSpeedable : public Ability {  //супербыстрый (Халк)
 public:
-    virtual void Run() = 0; //бегать
+    virtual void Run() = 0; // бегать
 };
 
-class IСopyingАppearanceable : public Ability {  //копирование внешности (Мистик)
+class IСopyingАppearanceable : public Ability {  // копирование внешности (Мистик)
 public:
-    virtual void Сopy() = 0; //копирование
+    virtual void Сopy() = 0; // копирование
 };
 
 class IEnduranceable : public Ability {  // выносливость (Халк) (Росомаха)
 public:
-    virtual void Fight() = 0; //драться
+    virtual void Fight() = 0; // драться
 };
 
 class IDexterityable : public Ability {  // ловкость (Халк) (Росомаха)
 public:
-    virtual void Catch() = 0; //ловить
+    virtual void Catch() = 0; // ловить
 };
 
 class IHeightenedSensesable : public Ability {  // oбострение чувств (Росомаха)
 public:
-    virtual void Look() = 0; //смотреть
-    virtual void Smell() = 0; //нюхать
+    virtual void Look() = 0; // смотреть
+    virtual void Smell() = 0; // нюхать
 };
 
 class IRegenerationable : public Ability {  // регенерация (Грут)
 public:
-    virtual void SkinRegeneration() = 0; //регенерировать ткани
-    virtual void LimbRegeneration() = 0;//регенерировать конечности
+    virtual void SkinRegeneration() = 0; // регенерировать ткани
+    virtual void LimbRegeneration() = 0; // регенерировать конечности
 };
 
 class IElasticityable : public Ability { // Эластичность (Грут)
 public:
-    virtual void StretchLimbs() = 0;//растягивать конечности
+    virtual void StretchLimbs() = 0; // растягивать конечности
 };
 
 class IIdealPersonable : public Ability {  // Идеальный человек (Капитан Америка)
 public:
-    virtual void Run() = 0; //бегать
-    virtual void Jump() = 0; //прыгать
-    virtual void Fight() = 0; //драться
+    virtual void Run() = 0; // бегать
+    virtual void Jump() = 0; // прыгать
+    virtual void Fight() = 0; // драться
 };
 
 class IDarknessManipulationable : public Ability {  // Манипуляции с тьмой (Тор)
 public:
-    virtual void Hypnosis() = 0; //гипноз
+    virtual void Hypnosis() = 0; // гипноз
 };
 
 ////////////////////////////////////////////////////////////////////////
 
-class Mystic : public SuperHero, Good, Human, Animal, public IСopyingАppearanceable { // Мистик
+class Mystic : public SuperHero, public IGood, public Human, public Animal, public IСopyingАppearanceable { // Мистик
 public:
-   virtual void Copy() {
-       cout << "Идет копирование в Мистик.../n";
+    Mystic() {
+        cout << "Mystic создалась...\n";
+    }
+    virtual void DoNiceThings() { cout << "some nice things that Mystic do\n"; }
+
+    virtual void Copy() {
+        cout << "Идет копирование в Мистик...\n";
     }
 };
 
-class Wolverine : public SuperHero, Good, Human, Animal, public IEnduranceable, IDexterityable, IHeightenedSensesable { // Росомаха
+class Wolverine : public SuperHero, public IGood, public Human, public Animal, public IEnduranceable, public IDexterityable, public IHeightenedSensesable { // Росомаха
 public:
+    Wolverine() {
+        cout << "Wolverine создался...\n";
+    }
+    virtual void DoNiceThings() { cout << "some nice things that Wolverine do\n"; }
+
     virtual void Fight() {
-        cout << "Росомаха наваляет плохим парням.../n";
+        cout << "Росомаха наваляет плохим парням...\n";
     }
 
     virtual void Catch() {
-        cout << "Росомаха ловит муху.../n";
+        cout << "Росомаха ловит муху...\n";
     }
 
     virtual void Look() {
-        cout << "Росомаха смотрит вокруг.../n";
+        cout << "Росомаха смотрит вокруг...\n";
     }
 
     virtual void Smell() {
-        cout << "Росомаха нюхает.../n";
+        cout << "Росомаха нюхает...\n";
     }
 };
 
-class TonyStark : public SuperHero, Good, Human, Animal, public IFlyable, ISuperStrongable { // Тони Старк
+class TonyStark : public SuperHero, public IGood, public Human, public Animal, public IFlyable, public ISuperStrongable { // Тони Старк
 public:
+    TonyStark() {
+        cout << "TonyStark создался...\n";
+    }
+    virtual void DoNiceThings() { cout << "some nice things that TonyStark do\n"; }
+
     virtual void Fly() {
-        cout << "Тони Старк летает.../n";
+        cout << "Тони Старк летает...\n";
     }
 
     virtual void WeightLifting() {
-        cout << "Тони Старк поднимает машину.../n";
+        cout << "Тони Старк поднимает машину...\n";
     }
 };
 
-class Groot : public SuperHero, Good, Human, Animal, public IRegenerationable, IElasticityable { // Грут
+class Groot : public SuperHero, public IGood, public Human, public Animal, public IRegenerationable, public IElasticityable { // Грут
 public:
-    virtual void SkinRegeneration () {
-        cout << "Грут регенерирует кожу.../n";
+    Groot() {
+        cout << "Groot создался...\n";
     }
-     
-    virtual void LimbRegeneration (){
-        cout << "Грут регенерирует ногу.../n";
+    virtual void DoNiceThings() { cout << "some nice things that Groot do\n"; }
+
+    virtual void SkinRegeneration() {
+        cout << "Грут регенерирует кожу...\n";
+    }
+
+    virtual void LimbRegeneration() {
+        cout << "Грут регенерирует ногу...\n";
     }
 
     virtual void StretchLimbs() {
-        cout << "Грут растягивает руки.../n";
+        cout << "Грут растягивает руки...\n";
     }
 };
 
-class Hulk : public SuperHero, Good, Human, public ISuperStrongable, ISuperSpeedable, IEnduranceable, IDexterityable { // Халк
+class Hulk : public SuperHero, public IGood, public Human, public ISuperStrongable, public ISuperSpeedable, public IEnduranceable, public IDexterityable { // Халк
 public:
+    Hulk() {
+        cout << "Hulk создался...\n";
+    }
+    virtual void DoNiceThings() { cout << "some nice things that Hulk do\n"; }
+
     virtual void WeightLifting() {
-        cout << "Халк поднимает слона.../n";
+        cout << "Халк поднимает слона...\n";
     }
 
     virtual void Run() {
-        cout << "Халк убегает.../n";
+        cout << "Халк убегает...\n";
     }
 
     virtual void Fight() {
-        cout << "Халк дерется с кем-то.../n";
+        cout << "Халк дерётся с кем-то...\n";
     }
 
     virtual void Catch() {
-        cout << "Халк ловит падающюю звезду.../n";
+        cout << "Халк ловит падающюю звезду...\n";
     }
 };
 
-class СapitanAmerica : public SuperHero, Good, Human, public IIdealPersonable { // Капитан Америка
+class СapitanAmerica : public SuperHero, public IGood, public Human, public IIdealPersonable { // Капитан Америка
 public:
+    СapitanAmerica() {
+        cout << "СapitanAmerica создался...\n";
+    }
+    virtual void DoNiceThings() { cout << "some nice things that СapitanAmerica do\n"; }
+
     virtual void Run() {
-        cout << "Капитан Америка доганяет поезд.../n";
+        cout << "Капитан Америка догоняет поезд...\n";
     }
 
     virtual void Fight() {
-        cout << "Капитан Америка дерется с кем-то.../n";
+        cout << "Капитан Америка дерётся с кем-то...\n";
     }
 
     virtual void Jump() {
-        cout << "Капитан Америка прыгает.../n";
+        cout << "Капитан Америка прыгает...\n";
     }
 };
 
 class Thor : public SuperHero, public IDarknessManipulationable { // Тор
 public:
+    virtual void DoNiceThings() { cout << "some nice things that Thor do\n"; }
+
     Thor() {
-        cout << "Тор создался.../n";
+        cout << "Тор создался...\n";
     }
-    virtual void Gipnosis() {
-        cout << "Тор манипулирует темной сущностью человека.../n";
+    // вот это я долго искал :)) Gipnosis / Hypnosis :))
+    virtual void Hypnosis() {
+        cout << "Тор манипулирует тёмной сущностью человека...\n";
     }
 };
 
 int main()
 {
     setlocale(0, "");
-    //Thor Thor;
-    SuperHero* s;
-    //s->AddSuperHero(&Thor);
 
-    //s = new Thor;
+   // Mystic mystic;
+    //mystic.Copy();
+    Wolverine wolverine;
+    wolverine.Catch();
+    TonyStark tony;
+    tony.Fly();
+    Hulk hulk;
+    hulk.Run();
+    Groot groot;
+    groot.LimbRegeneration();
+    СapitanAmerica cap;
+    cap.Run();
+    Thor thor;
+    thor.Hypnosis();
+
+    vector <SuperHero*> heroes;
     
-}
+    heroes.push_back(&thor);
+    heroes.push_back(&tony);
+    heroes.push_back(&hulk);
+    heroes.push_back(&groot);
+    heroes.push_back(&cap);
+    heroes.push_back(&wolverine);
+    
+    
+    for (auto hero : heroes) { 
+        IFlyable* f = dynamic_cast <IFlyable*> (hero);
+        if (f != nullptr) {
+            f->Fly();
+        }
+    }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+  }
